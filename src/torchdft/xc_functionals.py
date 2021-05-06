c526ae5f2d5e288980e5ecfff0d9bdd6311be9b4
Pablo del Mazo Sevillano <delmazosevillanopablo@gmail.com>
2021-05-06 14:43:42 +0200

    LDA exchange and correlation for exponential coulomb

--- src/torchdft/xc_functionals.py
+++ src/torchdft/xc_functionals.py
@@ -9,25 +9,52 @@ import torch
 from torchdft import constants
 
 
-def get_LDAX_potential(density, A=constants.A, kappa=constants.kappa, thres=1e-15):
+def get_exponential_coulomb_LDAX_potential(
+    density, A=constants.A, kappa=constants.kappa, thres=1e-15
+):
     """Evaluate exchange potential.
 
     Evaluate exchange potential.
-    TODO: I think the equation used in jax_dft for the exchange energy per
-    electron (derived from [1]) is not the same as the one given in [2] (this
-    one is provided explicitly for the electron per energy).
-    I will use the one in [2] multiplied by the prefactor A of the exponential
-    coulomb interaction, but this has to be confirmed!!
-
-
     [1] https://arxiv.org/pdf/1504.05620.pdf eq 17
-    [2] https://arxiv.org/pdf/1408.2434.pdf eq 11
     """
 
-    beta = 2.0 * density * math.pi / kappa
+    y = density * math.pi / kappa
     return torch.where(
         density > thres,
-        (A / (2.0 * math.pi))
-        * (torch.log(1.0 + beta * beta) / beta - 2 * torch.arctan(beta)),
-        (A / (2.0 * math.pi)) * (-beta + beta ** 3 / 6),
+        (A / (2.0 * math.pi)) * (torch.log(1.0 + y * y) / y - 2 * torch.arctan(y)),
+        (A / (2.0 * math.pi)) * (-y + y ** 3 / 6),
     )
+
+
+def get_exponential_coulomb_LDAC_potential(
+    density, A=constants.A, kappa=constants.kappa, thres=1e-15
+):
+    """Evaluate correlation potential.
+
+    Evaluate exchange potential.
+    """
+    y = density * math.pi / kappa
+    alpha = 2.0
+    beta = -1.00077
+    gamma = 6.26099
+    delta = -11.9041
+    eta = 9.62614
+    sigma = -1.48334
+    nu = 1.0
+
+    finite_y = torch.where(y == 0.0, torch.ones(1), y)
+    out = (
+        -A
+        * finite_y
+        / math.pi
+        / (
+            alpha
+            + beta * torch.sqrt(finite_y)
+            + gamma * finite_y
+            + delta * finite_y ** 1.5
+            + eta * finite_y ** 2
+            + sigma * finite_y ** 2.5
+            + nu * math.pi * kappa ** 2 / A * finite_y ** 3
+        )
+    )
+    return torch.where(y == 0.0, -A * y / math.pi / alpha, out)
--- tests/test_xc_functionals.py
+++ tests/test_xc_functionals.py
@@ -2,23 +2,39 @@ import unittest
 
 import torch
 
-from torchdft.xc_functionals import get_LDAX_potential
+from torchdft.xc_functionals import (
+    get_exponential_coulomb_LDAC_potential,
+    get_exponential_coulomb_LDAX_potential,
+)
 
 
 class XcFunctionalsTest(unittest.TestCase):
-    def test_get_LDAX_potential(self):
+    def test_get_exponential_coulomb_LDAX_potential(self):
         density = torch.Tensor([0.0, 1e-15, 1e-10, 1.0, 5.0])
 
-        p1 = get_LDAX_potential(density)
+        p1 = get_exponential_coulomb_LDAX_potential(density)
         p2 = torch.Tensor(
             [
                 0.0,
-                -5.110816343550001e-15,
-                -5.110816343550001e-10,
-                -0.4512822906001278,
-                -0.5114539643344302,
+                -2.5554081717750003e-15,
+                -2.5554081717750006e-10,
+                -0.398358052497694,
+                -0.49356793671356225,
             ]
         )
-        print(p1)
-        print(p2)
+        self.assertTrue(torch.allclose(p1, p2))
+
+    def test_get_exponential_coulomb_LDAC_potential(self):
+        density = torch.Tensor([0.0, 1e-15, 1e-10, 1.0, 5.0])
+
+        p1 = get_exponential_coulomb_LDAC_potential(density)
+        p2 = torch.Tensor(
+            [
+                -0.0,
+                -1.2777041412333279e-15,
+                -1.2777215850260872e-10,
+                -0.00771069759180703,
+                -0.0005034570738508356,
+            ]
+        )
         self.assertTrue(torch.allclose(p1, p2))
