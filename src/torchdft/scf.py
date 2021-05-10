c000424720e2d5bb1ba27040e2f7e19cc2a45d11
Jan Hermann <dev@jan.hermann.name>
2021-05-10 22:10:44 +0200

    simplify scf.py

--- src/torchdft/scf.py
+++ src/torchdft/scf.py
@@ -6,8 +6,6 @@ import torch
 
 from torchdft.functionals import (
     get_external_potential,
-    get_external_potential_energy,
-    get_hartree_energy,
     get_hartree_potential,
     get_kinetic_matrix,
     get_XC_energy,
@@ -16,72 +14,18 @@ from torchdft.functionals import (
 from torchdft.utils import exp_coulomb, get_dx
 from torchdft.xc_functionals import exponential_coulomb_LDA_XC_energy_density
 
-
-def get_effective_potential(ext_pot, density, grid, interaction_fn, XC_energy_density):
-    """Evaluate the effective potential of the system."""
-    return (
-        ext_pot
-        + get_hartree_potential(density, grid, interaction_fn)
-        + get_XC_potential(density, grid, XC_energy_density)
-    )
-
-
-def get_hamiltonian_matrix(grid, veff):
-    """Evaluate the hamiltonian matrix."""
-    return get_kinetic_matrix(grid) + torch.diag_embed(veff)
-
-
-def get_density_from_wf(nelectrons, grid, eigstates):
-    """Evaluate the density from a set of orbitals."""
-    dx = get_dx(grid)
-
-    # Only nelectrons / 2 spatial orbitals are occupied:
-    nocc = nelectrons // 2
-    occ = eigstates[:, :nocc]
-
-    occ = occ / torch.sqrt(torch.sum(occ * occ, axis=0) * dx)
-
-    occ = torch.column_stack((occ ** 2,) * 2)
-    return torch.sum(occ, axis=1)
-
-
-def get_total_eigen_ener(nelectrons, eigener):
-    """Evaluate the sum of eigen energies for the occupied states."""
-    nocc = nelectrons // 2
-    occ = eigener[:nocc]
-    occ = torch.column_stack((occ,) * 2)
-    return torch.sum(occ)
-
-
-def get_electronic_energy(
-    system, eigen_ener, veff, grid, density, interaction_fn, XC_energy_density
-):
-    """Evaluate the total energy of the system."""
-    E_veff = get_external_potential_energy(veff, density, grid)
-    K_ener = eigen_ener - E_veff
-
-    return (
-        K_ener
-        + get_external_potential_energy(system.vext, density, grid)
-        + get_hartree_energy(density, grid, interaction_fn)
-        + get_XC_energy(density, grid, XC_energy_density)
-    )
+__all__ = ["solve_ks"]
 
 
 def ks_iteration(nelectrons, veff, grid):
-    """Run one iteration to solve the KS equations."""
-    # Generate Hamiltonian matrix.
-    H = get_hamiltonian_matrix(grid, veff)
-
-    # Solve eigenstates:
+    H = get_kinetic_matrix(grid) + torch.diag_embed(veff)
     eigener, eigstates = torch.linalg.eigh(H)
-
-    # Generate new density:
-    new_density = get_density_from_wf(nelectrons, grid, eigstates)
-
-    # Total eigen energy:
-    total_eigener = get_total_eigen_ener(nelectrons, eigener)
-
+    nocc = nelectrons // 2
+    eigener, eigstates = eigener[:nocc], eigstates[:, :nocc]
+    dx = get_dx(grid)
+    eigstates = eigstates / ((eigstates ** 2).sum(dim=0) * dx).sqrt()  # normalize
+    new_density = (2 * (eigstates ** 2)).sum(dim=-1)
+    total_eigener = (2 * eigener).sum()
     return new_density, total_eigener
 
 
@@ -95,6 +39,7 @@ def solve_ks(
     print_iterations=False,
 ):
     """Given a system, evaluates its energy solvig the KS equations."""
+    dx = get_dx(grid)
 
     # Get external potential.
     system = system._replace(
@@ -114,20 +59,12 @@ def solve_ks(
         # TODO: Check if this copy is absolutely neccessary.
         old_density = new_density.clone().detach()
         old_ener = new_ener
-
-        veff = get_effective_potential(
-            system.vext, old_density, grid, interaction_fn, XC_energy_density
-        )
+        v_H = get_hartree_potential(old_density, grid, interaction_fn)
+        E_xc = get_XC_energy(old_density, grid, XC_energy_density)
+        v_xc = get_XC_potential(old_density, grid, XC_energy_density)
+        veff = system.vext + v_H + v_xc
         density, total_eigener = ks_iteration(system.nelectrons, veff, grid)
-        new_ener = get_electronic_energy(
-            system,
-            total_eigener,
-            veff,
-            grid,
-            density,
-            interaction_fn,
-            XC_energy_density,
-        )
+        new_ener = total_eigener + E_xc - ((v_H / 2 + v_xc) * old_density).sum() * dx
         system = system._replace(density=density)
         system = system._replace(energy=new_ener)
 
