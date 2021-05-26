6c919185280029d463aee315483ac478e9f84cf0
Jan Hermann <dev@jan.hermann.name>
2021-05-26 14:38:06 +0200

    add xitorch for diagonalization

--- src/torchdft/scf.py
+++ src/torchdft/scf.py
@@ -4,6 +4,8 @@
 from typing import Dict, List, Tuple, Union
 
 import torch
+import xitorch
+import xitorch.linalg
 from torch import Tensor
 
 from .basis import Basis
@@ -15,12 +17,18 @@ from .utils import GeneralizedDiagonalizer
 __all__ = ["solve_scf"]
 
 
-def ks_iteration(F: Tensor, X: Tensor, occ: Tensor) -> Tuple[Tensor, Tensor]:
+def ks_iteration(
+    F: Tensor, S: Tensor, occ: Tensor, use_xitorch: bool = False
+) -> Tuple[Tensor, Tensor]:
     n_occ = occ.shape[-1]
-    epsilon, C = GeneralizedDiagonalizer.eigh(F, X)
-    epsilon, C = epsilon[..., :n_occ], C[..., :n_occ]
+    if use_xitorch:
+        F, S = (xitorch.LinearOperator.m(x, is_hermitian=True) for x in [F, S])
+        epsilon, C = xitorch.linalg.symeig(F, n_occ, "lowest", S)
+    else:
+        epsilon, C = GeneralizedDiagonalizer.eigh(F, S)
+        epsilon, C = epsilon[..., :n_occ], C[..., :n_occ]
     P = (C * occ[..., None, :]) @ C.transpose(-2, -1)
-    energy_orb = (epsilon * occ).sum(-1)
+    energy_orb = (epsilon * occ).sum(dim=-1)
     return P, energy_orb
 
 
@@ -37,12 +45,14 @@ def solve_scf(
     enforce_symmetry: bool = False,
     log_dict: Dict[str, List[Tensor]] = None,
     create_graph: bool = False,
+    use_xitorch: bool = True,
 ) -> Tuple[Tensor, Tensor]:
     """Given a system, evaluates its energy by solving the KS equations."""
     S, T, V_ext = basis.get_core_integrals()
-    S = GeneralizedDiagonalizer(S)
+    if not use_xitorch:
+        S = GeneralizedDiagonalizer(S).X
     F = T + V_ext
-    P_in, energy_orb = ks_iteration(F, S.X, occ)
+    P_in, energy_orb = ks_iteration(F, S, occ, use_xitorch=use_xitorch)
     if enforce_symmetry and isinstance(basis, GridBasis):
         P_in = basis.symmetrize_P(P_in)
     energy_prev = energy_orb + basis.E_nuc
@@ -57,7 +67,7 @@ def solve_scf(
             P_in, xc_functional, create_graph=create_graph
         )
         F = T + V_ext + V_H + V_xc
-        P_out, energy_orb = ks_iteration(F, S.X, occ)
+        P_out, energy_orb = ks_iteration(F, S, occ, use_xitorch=use_xitorch)
         # TODO duplicate, should be made part of ks_iteration()
         if enforce_symmetry and isinstance(basis, GridBasis):
             P_out = basis.symmetrize_P(P_out)
