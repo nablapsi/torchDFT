ce15bbbded0f0e9142c83ad19156ac0aa594594b
Pablo del Mazo Sevillano <delmazosevillanopablo@gmail.com>
2021-06-16 10:58:28 +0200

    turn Density into dataclass

--- src/torchdft/density.py
+++ src/torchdft/density.py
@@ -1,14 +1,16 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from dataclasses import dataclass
 from typing import Optional
 
 from torch import Tensor
 
 
+@dataclass
 class Density:
     """Density data structure."""
 
-    def __init__(self, density: Tensor, grad: Tensor = None):
-        self.value = density
-        self.grad = grad
+    value: Tensor
+    grad: Optional[Tensor] = None
