d07659b4c51662ca5840ff47448dd3cf8963c85d
Pablo del Mazo Sevillano <delmazosevillanopablo@gmail.com>
2021-06-17 11:01:50 +0200

    add nn xc functionals

---
+++ src/torchdft/nn_functionals.py
@@ -0,0 +1,189 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from typing import Callable, List, Optional
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+from torchdft.density import Density
+from torchdft.functional import Functional
+from torchdft.gridbasis import get_hartree_potential
+from torchdft.utils import get_dx
+
+
+class SigLayer(nn.Module):
+    """Self Interaction Gate (SIG) layer."""
+
+    def __init__(self, grid: Tensor, interaction_fn: Callable[[Tensor], Tensor]):
+        super().__init__()
+        self.grid = grid
+        self.interaction_fn = interaction_fn
+        self.dx = get_dx(self.grid)
+        self.sigma = nn.Parameter(torch.Tensor(1))
+
+        # TODO: Check if this should be initialize from another distribution.
+        nn.init.uniform_(self.sigma)
+
+    def forward(self, density: Tensor, xc_energy_density: Tensor) -> Tensor:
+        density = density.squeeze()
+        if not hasattr(self, "Ne"):
+            self.Ne = density.detach().sum(-1) * self.dx
+        beta = (-(((self.Ne - 1) / self.sigma) ** 2)).exp()[:, None]
+        V_H = get_hartree_potential(density, self.grid, self.interaction_fn)
+        # NOTE: Why the 5e-1 for the Hartree energy density?
+        return xc_energy_density * (1 - beta) - 5e-1 * V_H * beta
+
+
+class GlobalConvolutionalLayer(nn.Module):
+    """Global convolutional layer."""
+
+    def __init__(
+        self, channels: int, grid: Tensor, minval: float = 0e0, maxval: float = 1e0
+    ):
+        super().__init__()
+        self.channels = channels
+        self.dx = get_dx(grid)
+        self.g = ((grid[:, None] - grid) ** 2).sqrt()
+        self.maxval = maxval
+        self.minval = minval
+        self.xi = nn.Parameter(torch.Tensor(self.channels))
+
+        nn.init.uniform_(self.xi)
+
+    def forward(self, density: Tensor) -> Tensor:
+        """Forward pass of global convolutional layer.
+
+        Args:
+            density: torch tensor of dimension(Nbatch, 1, grid_dim)
+            grid: torch tensor of dimension(grid_dim,)
+        """
+        xi = 1 / (self.minval + (self.maxval - self.minval) * torch.sigmoid(self.xi))
+        expo = (-(self.g[None, :, :] * xi[:, None, None])).exp()
+        integral = torch.einsum("i, jkl, ilm -> jim", 5e-1 * xi, density, expo)
+
+        return integral * self.dx
+
+
+class Conv1dPileLayers(nn.Module):
+    """Generate a pile of Conv1d layers."""
+
+    def __init__(
+        self, channels: List[int], kernels: List[int], negative_transform: bool = True
+    ):
+        super().__init__()
+
+        # TODO: Should I create dedicated errors for the asserts?
+        assert len(kernels) + 1 == len(channels)
+        self.requires_grad = False
+        self.transfer = nn.SiLU()
+        self.layers = nn.ModuleList()
+
+        for i, (channel, kernel) in enumerate(zip(channels[:-1], kernels)):
+            # NOTE: Only integer number kernels can be used to ensure input and
+            # output dimensions are kept constant.
+            assert kernel % 2 == 1
+            padding = (kernel - 1) // 2
+            self.layers.append(
+                nn.Conv1d(
+                    in_channels=channel,
+                    out_channels=channels[i + 1],
+                    kernel_size=kernel,
+                    padding=padding,
+                    bias=False,
+                )
+            )
+        self.sign = -1 if negative_transform else 1
+
+    def forward(self, x: Tensor) -> Tensor:
+        for layer in self.layers:
+            x = self.transfer(layer(x))
+
+        return (self.sign * x).squeeze()
+
+
+class Conv1dFunctionalNet(Functional):
+    """Convolutional 1D NN functional.
+
+    Creates LDA and GGA functionals upon the following instantiation.
+    LDA: window_size = 1, channels = [1, 16, 16, 16, 1], negative_transform = True
+    GGA: window_size = 3, channels = [1, 16, 16, 16, 1], negative_transform = True
+    """
+
+    def __init__(
+        self, window_size: int, channels: List[int], negative_transform: bool = True
+    ):
+        super().__init__()
+
+        self.requires_grad = False
+
+        kernels = [1] * (len(channels) - 1)
+        kernels[0] = window_size
+
+        self.conv1d = Conv1dPileLayers(channels, kernels, negative_transform)
+
+    def forward(self, den: Density) -> Tensor:
+        # NOTE: Anyway to do this without if?
+        if len(den.value.shape) == 2:
+            x = den.value[:, None, :]
+        else:
+            x = den.value[None, None, :]
+
+        return self.conv1d(x)
+
+
+class GlobalFunctionalNet(Functional):
+    """Global convolutional 1d NN functional."""
+
+    def __init__(
+        self,
+        channels: List[int],
+        glob_channels: int,
+        grid: Tensor,
+        kernels: List[int],
+        maxval: float = 1e0,
+        minval: float = 0e0,
+        negative_transform: bool = True,
+        sig_layer: bool = True,
+        interaction_fn: Optional[Callable[[Tensor], Tensor]] = None,
+    ):
+        super().__init__()
+
+        self.requires_grad = False
+
+        self.channels = channels
+        self.grid = grid
+        self.interaction_fn = interaction_fn
+        self.kernels = kernels
+        self.maxval = maxval
+        self.minval = minval
+        self.negative_transform = negative_transform
+        self.sig_layer = sig_layer
+
+        self.globalconv = GlobalConvolutionalLayer(
+            glob_channels, self.grid, maxval=self.maxval, minval=self.minval
+        )
+        self.conv1d = Conv1dPileLayers(
+            kernels=self.kernels,
+            channels=self.channels,
+            negative_transform=self.negative_transform,
+        )
+        if self.sig_layer:
+            assert self.interaction_fn is not None
+            self.sig = SigLayer(self.grid, self.interaction_fn)
+
+    def forward(self, den: Density) -> Tensor:
+        if len(den.value.shape) == 2:
+            density = den.value[..., None, :]
+        else:
+            density = den.value[None, None, :]
+
+        x = self.globalconv(density)
+        x = self.conv1d(x)
+
+        if self.sig_layer:
+            x = self.sig(density, x)
+
+        return x.squeeze()
---
+++ tests/test_nn_functionals.py
@@ -0,0 +1,136 @@
+import torch
+from torch.testing import assert_allclose
+
+from torchdft.density import Density
+from torchdft.gridbasis import get_hartree_potential
+from torchdft.nn_functionals import (
+    Conv1dFunctionalNet,
+    Conv1dPileLayers,
+    GlobalConvolutionalLayer,
+    GlobalFunctionalNet,
+    SigLayer,
+)
+from torchdft.utils import exp_coulomb, gaussian
+
+
+class TestSigLayer:
+    grid = torch.arange(-10, 10, 1)
+    density = gaussian(grid, 0, 1)
+
+    def test_forward(self):
+        layer = SigLayer(self.grid, exp_coulomb)
+        xc_energy_density = torch.rand(self.grid.shape)
+
+        _ = layer(self.density, xc_energy_density)
+
+    def test_1e(self):
+        layer = SigLayer(self.grid, exp_coulomb)
+        xc_energy_density = torch.rand(self.grid.shape)
+
+        out = layer(self.density, xc_energy_density)
+        V_H = get_hartree_potential(self.density, self.grid, exp_coulomb)
+
+        assert_allclose(out.squeeze(), -5e-1 * V_H)
+
+
+class TestGlobalConvolutionalLayer:
+    def test_forward(self):
+        nbatch, ngrid, nchannels = 2, 3, 4
+        grid = torch.rand(ngrid)
+        density = torch.rand(nbatch, 1, ngrid)
+
+        gconvlayer = GlobalConvolutionalLayer(nchannels, grid)
+
+        value = gconvlayer(density)
+
+        xi = 1 / (
+            gconvlayer.minval
+            + (gconvlayer.maxval - gconvlayer.minval) * torch.sigmoid(gconvlayer.xi)
+        )
+        # Unrolled calculation.
+        a = torch.zeros([nchannels, ngrid, ngrid])
+        for i in range(nchannels):
+            for j in range(ngrid):
+                for k in range(ngrid):
+                    a[i, j, k] += gconvlayer.g[j, k] * xi[i]
+
+        a = (-a).exp()
+
+        uvalue = torch.zeros(nbatch, nchannels, ngrid)
+        for i in range(nbatch):
+            for j in range(nchannels):
+                for k in range(ngrid):
+                    for l in range(ngrid):
+                        uvalue[i, j, l] += density[i, 0, k] * a[j, k, l] * xi[j]
+        uvalue *= 5e-1 * gconvlayer.dx
+
+        assert_allclose(value, uvalue)
+
+
+class TestConv1dPileLayers:
+    channels = [1, 16, 16, 1]
+    kernels = [1, 3, 1]
+
+    def test_forward(self):
+        layer = Conv1dPileLayers(self.channels, self.kernels, negative_transform=True)
+
+        density = torch.rand((1, 1, 10))
+        out = layer(density)
+
+        assert list(out.shape) == [10]
+
+    def test_forward_batch(self):
+        layer = Conv1dPileLayers(self.channels, self.kernels, negative_transform=True)
+
+        density = torch.rand((3, 1, 10))
+        out = layer(density)
+
+        assert list(out.shape) == [3, 10]
+
+
+class TestConv1dFunctionalNet:
+    def test_LDA_forward(self):
+        net = Conv1dFunctionalNet(1, [1, 16, 16, 1], negative_transform=True)
+        density = Density(torch.rand((3, 10)))
+
+        _ = net(density)
+
+    def test_GGA_forward(self):
+        net = Conv1dFunctionalNet(3, [1, 16, 16, 1], negative_transform=True)
+        density = Density(torch.rand((3, 10)))
+
+        _ = net(density)
+
+
+class TestGlobalFunctionalNet:
+    grid = torch.arange(-10, 10, 1)
+    density = Density(gaussian(grid, 0, 1))
+
+    def test_forward(self):
+        net = GlobalFunctionalNet(
+            channels=[4, 16, 1],
+            glob_channels=4,
+            grid=self.grid,
+            kernels=[3, 1],
+            maxval=1.0,
+            minval=0.0,
+            negative_transform=True,
+            sig_layer=False,
+        )
+
+        _ = net(self.density)
+
+    def test_SIG_forward(self):
+        net = GlobalFunctionalNet(
+            channels=[4, 16, 1],
+            glob_channels=4,
+            grid=self.grid,
+            kernels=[3, 1],
+            maxval=1.0,
+            minval=0.0,
+            negative_transform=True,
+            sig_layer=True,
+            interaction_fn=exp_coulomb,
+        )
+
+        _ = net(self.density)
