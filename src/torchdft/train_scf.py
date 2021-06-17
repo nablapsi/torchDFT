f8ea3b6cf00ca50ee06d0c0acee2f77c1cd401f1
Pablo del Mazo Sevillano <delmazosevillanopablo@gmail.com>
2021-06-17 11:07:01 +0200

    add training function

---
+++ src/torchdft/train_scf.py
@@ -0,0 +1,116 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from typing import Any, Callable, Dict, List, Tuple, TypeVar, Union
+
+import torch
+from torch import Tensor
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from .basis import Basis
+from .errors import SCFNotConverged
+from .functional import Functional
+from .scf import solve_scf
+from .utils import SystemBatch
+
+__all__ = ["train_functional"]
+
+T_co = TypeVar("T_co", covariant=True)
+
+
+def train_functional(
+    basis_class: Callable[[SystemBatch], Basis],
+    functional: Functional,
+    optimizer: torch.optim.Optimizer,
+    dataloader: DataLoader[T_co],
+    alpha_decay: float = 0.9,
+    checkpoint_freq: Union[bool, int] = False,
+    max_epochs: int = 10,
+    max_iterations: int = 15,
+    mode: str = "KS",
+    requires_closure: bool = False,
+    writer: SummaryWriter = None,
+    **kwargs: Any,
+) -> float:
+    """Train a functional."""
+    assert len(dataloader) == 1
+    systemlist, E_truth, n_truth = next(iter(dataloader))
+    basis_list, occ_list = [], []
+    for system in systemlist:
+        basis_list.append(basis_class(system))
+        occ_list.append(system.occ(mode=mode))
+    step = 0
+    for epoch in range(max_epochs):
+
+        def closure() -> float:
+            nonlocal step
+            optimizer.zero_grad()
+            losses = [
+                training_step(
+                    basis,
+                    occ,
+                    functional,
+                    *data,
+                    max_iterations=max_iterations,
+                    alpha_decay=alpha_decay,
+                    **kwargs,
+                )
+                for basis, occ, *data in zip(basis_list, occ_list, E_truth, n_truth)
+            ]
+            loss, E_loss, n_loss = (torch.stack(x).mean() for x in zip(*losses))
+            if writer:
+                writer.add_scalars(
+                    "Losses",
+                    {"E_loss": E_loss, "n_loss": n_loss, "Loss": loss},
+                    step,
+                )
+            if checkpoint_freq and step % checkpoint_freq == 0:
+                torch.save(functional.state_dict(), f"checkpoint_{epoch}.pth")
+            step += 1
+            return loss.item()
+
+        if requires_closure:
+            optimizer.step(closure)
+        else:
+            closure()
+            optimizer.step()
+
+    loss = closure()
+    return loss
+
+
+def training_step(
+    basis: Basis,
+    occ: Tensor,
+    functional: Functional,
+    E_truth: Tensor,
+    n_truth: Tensor,
+    trajectory_discount: float = 0.8,
+    initial_skip: int = 10,
+    **kwargs: Any,
+) -> Tuple[Tensor, Tensor, Tensor]:
+    log_dict: Dict[str, List[Tensor]] = {}
+    try:
+        solve_scf(
+            basis,
+            occ,
+            functional,
+            log_dict=log_dict,
+            create_graph=True,
+            **kwargs,
+        )
+    except SCFNotConverged:
+        pass
+    E_pred = torch.stack(log_dict["energy"][-initial_skip:])
+    n_pred = basis.density(log_dict["denmat"][-1])
+    w = trajectory_discount ** torch.arange(
+        len(E_pred) - 1, -1, -1, device=E_pred.device
+    )
+    N = occ.sum()
+    E_loss = (w * (E_pred - E_truth) ** 2).sum(-1) / N
+    n_loss = basis.density_mse(n_pred - n_truth) / N
+    loss = E_loss + n_loss
+    loss.backward()
+    return loss, E_loss, n_loss
---
+++ tests/test_train_functional.py
@@ -0,0 +1,105 @@
+import torch
+from torch.utils.data import DataLoader
+
+from torchdft.dataset import SystemDataSet, collate_fn
+from torchdft.gridbasis import GridBasis
+from torchdft.nn_functionals import Conv1dFunctionalNet, GlobalFunctionalNet
+from torchdft.train_scf import train_functional
+from torchdft.utils import System, exp_coulomb
+
+
+class TestTrainScf:
+    charges = torch.tensor([[1, 0, 1], [1, 1, 1]])
+    centers = torch.tensor([[-1, 0, 1], [1, 0, 1]])
+    n_electrons = torch.tensor([[2], [3]])
+    grid = torch.arange(-10, 10, 1)
+    E_truth = torch.rand(charges.shape[0])
+    D_truth = torch.rand((charges.shape[0], grid.shape[0]))
+
+    models = [
+        Conv1dFunctionalNet(
+            window_size=1, channels=[1, 16, 16, 1], negative_transform=True
+        ),
+        Conv1dFunctionalNet(
+            window_size=3, channels=[1, 16, 16, 1], negative_transform=True
+        ),
+        GlobalFunctionalNet(
+            channels=[4, 16, 1],
+            glob_channels=4,
+            grid=grid,
+            kernels=[3, 1],
+            maxval=1.0,
+            minval=0.0,
+            negative_transform=True,
+            sig_layer=False,
+        ),
+        GlobalFunctionalNet(
+            channels=[4, 16, 1],
+            glob_channels=4,
+            grid=grid,
+            kernels=[3, 1],
+            maxval=1.0,
+            minval=0.0,
+            negative_transform=True,
+            sig_layer=True,
+            interaction_fn=exp_coulomb,
+        ),
+    ]
+
+    systems = []
+    for i, charge in enumerate(charges):
+        systems.append(
+            System(
+                charges=charge,
+                n_electrons=int(n_electrons[i]),
+                centers=centers[i],
+                grid=grid,
+            )
+        )
+
+    systemds = SystemDataSet(systems, E_truth, D_truth)
+    dataloader = DataLoader(systemds, batch_size=len(systemds), collate_fn=collate_fn)
+
+    def test_train_scf(self):
+
+        for xc_nn in self.models:
+            optimizer = torch.optim.Adam(xc_nn.parameters(), lr=0.01)
+            train_functional(
+                basis_class=GridBasis,
+                functional=xc_nn,
+                optimizer=optimizer,
+                dataloader=self.dataloader,
+                max_epochs=1,
+            )
+
+            train_functional(
+                basis_class=GridBasis,
+                functional=xc_nn,
+                optimizer=optimizer,
+                dataloader=self.dataloader,
+                max_epochs=1,
+                enforce_symmetry=True,
+            )
+
+    def test_train_scf_closure(self):
+
+        for xc_nn in self.models:
+            optimizer = torch.optim.LBFGS(xc_nn.parameters(), lr=1, max_iter=1)
+            train_functional(
+                basis_class=GridBasis,
+                functional=xc_nn,
+                optimizer=optimizer,
+                dataloader=self.dataloader,
+                max_epochs=1,
+                requires_closure=True,
+            )
+
+            train_functional(
+                basis_class=GridBasis,
+                functional=xc_nn,
+                optimizer=optimizer,
+                dataloader=self.dataloader,
+                max_epochs=1,
+                requires_closure=True,
+                enforce_symmetry=True,
+            )
