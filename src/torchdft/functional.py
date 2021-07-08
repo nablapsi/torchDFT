32d06b260864420df4e8eb5b456e6597df51b887
Pablo del Mazo Sevillano <delmazosevillanopablo@gmail.com>
2021-07-08 15:28:49 +0200

    [ci] solve flake8

--- src/torchdft/functional.py
+++ src/torchdft/functional.py
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 from abc import abstractmethod
-from typing import Dict, List, Optional
+from typing import List, Optional
 
 import torch
 from torch import Tensor, nn
