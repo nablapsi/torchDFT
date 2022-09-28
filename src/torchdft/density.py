5aafe9e155ce2c5c42aea60a420023cc84351606
Pablo del Mazo Sevillano <delmazosevillanopablo@gmail.com>
2022-09-28 17:15:59 +0200

    store up/down electron density and total density in Density

--- src/torchdft/density.py
+++ src/torchdft/density.py
@@ -11,11 +11,22 @@ from torch import Tensor
 
 @dataclass
 class Density:
-    """Density data structure."""
+    """Density data structure.
 
+    Attributes:
+        - density: Total density. Sum of spin up and down densities.
+            shape = [batch, basis].
+        - value: Total density in spin unpolarized systems and up/down densities
+            in spin polarized systems. shape = [batch, (spin), basis].
+        - nu, nd: Up and down densities. shape = [batch, basis]
+    """
+
+    density: Tensor
     value: Tensor
     grid: Tensor
     grid_weights: Tensor
+    nd: Tensor
+    nu: Tensor
     grad: Optional[Tensor] = None
 
     def __init__(
@@ -34,3 +45,8 @@ class Density:
             self.value.new_full((1,), 1e-100),
             self.value,
         )
+        if len(self.value.shape) == 3:
+            self.nu, self.nd = self.value[..., 0, :], self.value[..., 1, :]
+        else:
+            self.nu = self.nd = self.value / 2
+        self.density = self.nu + self.nd
