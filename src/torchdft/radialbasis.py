632774c50e28cf9f75d6aff7198dbe2a0ea5189a
Pablo del Mazo Sevillano <delmazosevillanopablo@gmail.com>
2022-10-03 15:22:35 +0200

    apply changes on RadialGrid to work with batch of grids

--- src/torchdft/radialbasis.py
+++ src/torchdft/radialbasis.py
@@ -24,21 +24,20 @@ class RadialBasis(Basis):
     def __init__(self, system: Union[System, SystemBatch], grid: Grid):
         super().__init__()
         self.system = system
-        self.register_buffer("grid", grid.grid)
+        # In most cases grid is dividing.
+        # It is more convenient to change the zeros to infinity.
+        self.register_buffer("grid", torch.where(grid.grid == 0, torch.inf, grid.grid))
         self.register_buffer("dv", grid.dv)
         self.register_buffer("grid_weights", grid.grid_weights)
         self.register_buffer("Z", self.system.Z.squeeze(-1))
         self.register_buffer("E_nuc", self.Z.new_zeros(self.Z.shape))
         self.register_buffer("T", -5e-1 * self.get_laplacian())
         self.register_buffer("V_ext", (-self.system.Z / self.grid).diag_embed())
-        self.register_buffer(
-            "S",
-            torch.full_like(self.grid, 1.0, device=self.grid.device).diag_embed(),
-        )
+        self.register_buffer("S", self.grid.new_ones(self.grid.size()).diag_embed())
         self.register_buffer(
             "grad_operator",
-            fin_diff_matrix(self.grid.shape[0], 5, 1, dtype=self.grid.dtype)
-            / self.grid_weights,
+            fin_diff_matrix(self.grid.shape[-1], 5, 1, dtype=self.grid.dtype)
+            / self.grid_weights[..., None, None],
         )
 
     def get_core_integrals(self) -> Tuple[Tensor, Tensor, Tensor]:
@@ -72,7 +71,8 @@ class RadialBasis(Basis):
         """
         if not P.requires_grad:
             P = P.detach().requires_grad_()
-        density = Density(self.density(P), self.grid, self.dv * self.grid_weights)
+        grid = torch.where(self.grid.isinf(), self.grid.new_zeros(1), self.grid)
+        density = Density(self.density(P), grid, self.dv * self.grid_weights[..., None])
         if functional.requires_grad:
             density.grad = self.get_density_gradient(P)
 
@@ -80,7 +80,7 @@ class RadialBasis(Basis):
         eps_func = functional(density)
         if functional.per_electron:
             eps_func = eps_func * density.density
-        E_func = (eps_func * self.dv * self.grid_weights).sum(-1)
+        E_func = (eps_func * self.dv * self.grid_weights[..., None]).sum(-1)
         (v_func,) = torch.autograd.grad(
             eps_func.sum(), density.value, create_graph=create_graph
         )
@@ -109,13 +109,15 @@ class RadialBasis(Basis):
         return lap
 
     def density_mse(self, density: Tensor) -> Tensor:
-        return (density.pow(2) * self.dv * self.grid_weights).sum(dim=-1)
+        return (density.pow(2) * self.dv * self.grid_weights[..., None]).sum(dim=-1)
 
     def density(self, P: Tensor) -> Tensor:
-        return P.diagonal(dim1=-2, dim2=-1) / (self.dv * self.grid_weights)
+        n = P.diagonal(dim1=-2, dim2=-1) / (self.dv * self.grid_weights[..., None])
+        return torch.where(self.dv == 0, n.new_zeros(1), n)
 
     def quadrupole(self, density: Tensor) -> Tensor:
-        Q_el = -(self.grid**2 * density * self.dv * self.grid_weights).sum(-1)
+        grid = torch.where(self.grid.isinf(), self.grid.new_zeros(1), self.grid)
+        Q_el = -(grid**2 * density * self.dv * self.grid_weights[..., None]).sum(-1)
         return Q_el
 
     def density_metrics_fn(
@@ -128,7 +130,7 @@ class RadialBasis(Basis):
 
     def get_laplacian(self) -> Tensor:
         """Finite difference approximation of Laplacian operator."""
-        grid_dim = self.grid.size(0)
+        grid_dim = self.grid.size(-1)
         laplacian = (
             (-2.5 * self.grid.new_ones([grid_dim]).diag_embed())
             + (4.0 / 3.0 * self.grid.new_ones([grid_dim - 1]).diag_embed(offset=1))
@@ -141,12 +143,14 @@ class RadialBasis(Basis):
         laplacian[..., 0, 0] += (1 + self.grid_weights * self.Z) / (
             12 * (1 - self.grid_weights * self.Z)
         )
-        return laplacian / self.grid_weights**2
+        grid_weight_mask = self.dv[..., None, :] * self.dv[..., :, None] == 0.0
+        laplacian = torch.where(grid_weight_mask, laplacian.new_zeros(1), laplacian)
+        return laplacian / self.grid_weights[..., None, None] ** 2
 
     def get_hartree_potential(self, density: Tensor, grid: Tensor) -> Tensor:
         """Evaluate Hartree potential."""
         # https://gitlab.com/aheld84/radialdft/-/blob/master/radialdft/poisson.py
-        ndv = density * self.dv * self.grid_weights
+        ndv = density * self.dv * self.grid_weights[..., None]
         u_l = grid.new_zeros(density.shape)
         u_l[..., 0] = 0
         u_l[..., 1:] = torch.cumsum(ndv[..., :-1], dim=-1)
@@ -156,4 +160,4 @@ class RadialBasis(Basis):
 
     def get_psi(self, C: Tensor) -> Tensor:
         psi = C / self.grid_weights.sqrt()
-        return psi
+        return torch.where(self.grid_weights == 0e0, psi.new_zeros(1), psi)
